6f1c06a58af63740b42c836ca18c8d60c2f7f0d9
Guilherme Giachellin <guilherme.giache@gmail.com>
2022-07-12 18:02:55 -0300

    Add files via upload

---
+++ food.java
@@ -0,0 +1,10 @@
+import controller.SaController;
+
+public class food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SaController start = new SaController();
+	}
+
+}
